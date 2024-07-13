2145251d22fde621f246cf86773921b6909b5bcc
bjmgales <bjmgales@gmail.com>
2024-07-13 09:22:32 +0200

    add: ex08 - Norm + Test

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,21 +1,34 @@
-from time import sleep
+# from time import sleep
+# from tqdm import tqdm
+
+def ft_tqdm(lst: range) -> None:
+    """ft_tqdm(itterable) ->None
+
+    This function emulates the behavior of the original tqdm without using any
+    libraries.
+
+    Decorate an iterable object, returning an iterator which acts
+    exactly like the original iterable, but prints a dynamically updating
+    progressbar every time a value is requested. """
+    percent = 0
+    for i in range(lst.stop + 1):
+        print("\r" + str(percent) + "%|" + ("█" * percent) +
+              (" " * (100 - percent)) + f'| {i}/{lst.stop}',
+              end="", flush=True)
+        percent = round((100 * i) / (lst.stop - 1))
+        yield
 
 
-def ft_tqdm(lst: range):
-    toto = 0
-    modulo = (lst.stop / 100)
-    for i in lst:
-        if (i % modulo) == 0:
-            toto += 1
-            print(str(toto) + "%|" + ("=" * toto) + (" " * (100 - toto)) + "|", end="\r", flush=True)
-        yield toto
+#       Test        #
+
+# def main():
+#         print(tqdm.__doc__)
+#     for elem in ft_tqdm(range(2322)):
+#         sleep(0.01)
+#     print()
+#     for elem in tqdm(range(2322)):
+#         sleep(0.1)
 
 
-def main():
-
-    for elem in ft_tqdm(range(100)):
-        sleep(1)
-
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
