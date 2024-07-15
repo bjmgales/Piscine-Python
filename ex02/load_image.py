f58e5400c2899bda0c2dbbb2d3957497d6edd65f
bjmgales <bjmgales@gmail.com>
2024-07-15 11:10:10 +0200

    add: day01/ex02

---
+++ ex02/load_image.py
@@ -0,0 +1,25 @@
+import imageio.v2 as lio
+
+
+def ft_load(path: str) -> tuple:
+    try:
+        if not isinstance(path, str):
+            raise TypeError("TypeError: path parameter must be of str type")
+        img = lio.imread(path)
+    except (FileNotFoundError, ValueError, PermissionError) as msg:
+        print(type(msg).__name__ + ":", msg)
+        return None
+
+    print("The shape of the image is:", img.shape)
+    return img
+
+
+def main():
+    print(ft_load("./ex02/image.png"))
+    print(ft_load("./ex02/image.jpg"))
+    print(ft_load("./ex02/corrupted.jpg"))
+    print(ft_load("./ex02/permission.jpg"))
+
+
+if __name__ == "__main__":
+    main()
